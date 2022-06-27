90e1026e5035901d61c83b81abc2ea1412883b62
Natalia-Roldan <naty.roldan82@gmail.com>
2022-06-27 17:36:24 -0300

    update

---
+++ js/ciclo.js
@@ -0,0 +1,10 @@
+//FOR LOOP
+
+  let numero=  prompt( "Ingrese un numero");
+
+{ if (numero % 2 ===0){
+    document.write (`El nuemro ${numero} es Par`);}
+    else{
+        document.write (`El nuemro ${numero} es ImPar`);
+    }
+}
---
+++ js/index.js
@@ -0,0 +1,7 @@
+const nuevoNombre= document.querySelector("H1")
+nuevoNombre.textContent= 'Natalia Roldan / Aprendiendo JavaScript';
+console.log(nuevoNombre);
+
+
+
+    
